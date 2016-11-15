44ec705f621bb89933bae7cdf856f52776425129
Felipe Frizzo <felipefrizzo@gmail.com>
2016-11-15 19:44:24 -0200

    Create Root Screen

---
+++ src/main/java/br/univel/view/RootLayoutController.java
@@ -0,0 +1,14 @@
+package br.univel.view;
+
+import br.univel.Main;
+
+/**
+ * Created by felipefrizzo on 15/11/16.
+ */
+public class RootLayoutController {
+    private Main main;
+
+    public void setMain(final Main main) {
+        this.main = main;
+    }
+}
